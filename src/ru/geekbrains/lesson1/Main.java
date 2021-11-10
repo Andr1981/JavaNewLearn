f89214744d2da2d3febd56a7ea81bc5516e7e887
Андрей Парфенов <58725265+Andr1981@users.noreply.github.com>
2021-11-10 23:15:19 +0300

    Second commit

--- src/ru/geekbrains/lesson1/Main.java
+++ src/ru/geekbrains/lesson1/Main.java
@@ -1,8 +1,18 @@
 package ru.geekbrains.lesson1;
 
+import java.io.IOException;
+import java.io.Reader;
+
 public class Main {
+    public static int a = 5;
+    public static int b = 2;
+    public static int c;
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        if (a + b < 10) {
+            System.out.println("Привет");
+        }
     }
 }
